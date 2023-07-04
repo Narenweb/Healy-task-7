10b91b922bb260fbbe5690f3a6d21b2e04ab07d3
Narayanan M.S <107301043+Narenweb@users.noreply.github.com>
2023-07-04 16:10:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbar = document.getElementById('navbar');
     const navbarHeight = navbar.offsetHeight;
+    const closeIcon = document.querySelector('.close-icon');
+    const hamburger=document.querySelector('.hamburger');
   
+    // For scrolling event
     window.addEventListener('scroll', function() {
       const scrollPosition = window.scrollY;
 
@@ -9,6 +12,19 @@ document.addEventListener('DOMContentLoaded', function() {
         navbar.classList.add('navbar-fixed');
       } else {
         navbar.classList.remove('navbar-fixed');
+        navbar.classList.remove('slide-navbar');
       }
     });
+    
+     // For sliding event
+    hamburger.addEventListener('click', function (){
+       navbar.classList.add('slide-navbar');
+    });
+   
+    closeIcon.addEventListener('click', function() {
+      navbar.classList.remove('slide-navbar');
+    });
+    
   });
+ 
+
